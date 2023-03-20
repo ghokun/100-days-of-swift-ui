7451064d6b1a5485dadfc3ab1d088740ead3d6e6
Gokhun Celik <gokhun@gmail.com>
2023-03-20 06:50:28 +0100

    Day16 end

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by  Gokhun Celik on 19/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    Text("Hello, world!")
+      .padding()
+  }
+}
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by  Gokhun Celik on 19/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
